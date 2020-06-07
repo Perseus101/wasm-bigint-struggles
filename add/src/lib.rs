9559863d41ffe75e1f0648ab6f703025671e8274
Colin Moore <colin@moore.one>
2020-06-07 13:47:24 -0400

    Remove wasm-bindgen from add

--- add/src/lib.rs
+++ add/src/lib.rs
@@ -1,12 +1,10 @@
-use wasm_bindgen::prelude::*;
-
 mod sys {
     extern {
         pub fn native_add(a: u64, b: u64) -> u64;
     }
 }
 
-#[wasm_bindgen]
+#[no_mangle]
 pub fn add(a: u64, b: u64) -> u64 {
     unsafe { sys::native_add(a, b) }
 }
